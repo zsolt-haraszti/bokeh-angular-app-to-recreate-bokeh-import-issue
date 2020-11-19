9312964e0543adfc69a28c6c2fc7515dface97ed
Zsolt Haraszti <zharaszti@nvidia.com>
2020-11-19 14:48:16 -0800

    Single bokeh lib import reproducing the issue
    After installing @okeh/bokehjs-2.2.3, a single import LayoutDOM
    can reproduce the import issue, attributed to circular import
    dependencies.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {LayoutDOM} from '@bokeh/models/layouts/layout_dom';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bokeh-angular-app-to-recreate-bokeh-import-issue';
+
+  constructor() {
+    console.log('was able to load:', LayoutDOM);
+  }
 }
